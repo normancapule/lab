cce9ea7235bd50494f7aa013df742dc7fdf85e4b
John Norman Capule <johnnormancapule@gmail.com>
2020-03-30 01:27:59 +0800

    Elixir bootcamp - Section 2

--- elixir/udemy/cards/lib/cards.ex
+++ elixir/udemy/cards/lib/cards.ex
@@ -23,4 +23,22 @@ defmodule Cards do
   def contains?(deck, card) do
     Enum.member?(deck, card)
   end
+
+  def save(deck, filename) do
+    binary = :erlang.term_to_binary(deck)
+    File.write(filename, binary)
+  end
+
+  def load(filename) do
+    case File.read(filename) do
+      {:ok, binary} -> :erlang.binary_to_term(binary)
+      {:error, _reason} -> "File does not exist"
+    end
+  end
+
+  def create_hand(hand_size) do
+    create_deck
+    |> shuffle
+    |> deal(hand_size)
+  end
 end
