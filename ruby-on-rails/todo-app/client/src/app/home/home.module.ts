2debd67ca01b662ac5d621b7c5acf3b0f9154903
John Norman Capule <johnnormancapule@gmail.com>
2020-07-10 19:53:53 +0800

    Working routes + tailwind integration

--- ruby-on-rails/todo-app/client/src/app/home/home.module.ts
+++ ruby-on-rails/todo-app/client/src/app/home/home.module.ts
@@ -1,12 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import { HomeComponent } from './home.component';
+import { HomeRoutingModule } from './home-routing.module';
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  imports: [CommonModule, HomeRoutingModule],
+  declarations: [HomeComponent]
 })
+
 export class HomeModule { }
--- ruby-on-rails/todo-app/client/src/app/login/login.module.ts
+++ ruby-on-rails/todo-app/client/src/app/login/login.module.ts
@@ -1,12 +1,11 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import { NgModule } from '@angular/core';
+import { LoginComponent } from './login.component';
+import { LoginRoutingModule } from './login-routing.module';
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  imports: [CommonModule, LoginRoutingModule],
+  declarations: [LoginComponent]
 })
+
 export class LoginModule { }
