92efa36ff3770aefe8fb9ab0ba1794b65a96c97f
John Norman Capule <johnnormancapule@gmail.com>
2020-11-02 05:57:43 +0800

    Cleanups. Remove unused sections

--- ruby-on-rails/todo-app/api/app/controllers/application_controller.rb
+++
@@ -1,5 +0,0 @@
-class ApplicationController < ActionController::API
-  rescue_from StandardError do |exception|
-    render json: { error: exception.message }, status: :unprocessable_entity
-  end
-end
--- ruby-on-rails/todo-app/api/config/routes.rb
+++
@@ -1,9 +0,0 @@
-Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  namespace :api, defaults: { format: 'json' } do
-    scope module: :v1 do
-      resources :sessions, only: %w[create destroy]
-      post '/registrations', to: 'registrations#create'
-    end
-  end
-end
